c9a8902fb933da19408f7329ee61efa66e0122e4
rxm <33541942+rxm96@users.noreply.github.com>
2026-01-14 23:12:31 +0100

    feat: add more twitch android user agents, choose random on app start

--- src/main/config.ts
+++ src/main/config.ts
@@ -6,9 +6,23 @@ export const TWITCH_OAUTH_DEVICE_URL = "https://id.twitch.tv/oauth2/device";
 export const TWITCH_OAUTH_TOKEN_URL = "https://id.twitch.tv/oauth2/token";
 export const TWITCH_ACTIVATE_URL = "https://www.twitch.tv/activate";
 
+const TWITCH_ANDROID_USER_AGENTS = [
+  "Dalvik/2.1.0 (Linux; U; Android 16; SM-S911B Build/TP1A.220624.014) tv.twitch.android.app/25.3.0/2503006",
+  "Dalvik/2.1.0 (Linux; U; Android 16; SM-S938B Build/BP2A.250605.031) tv.twitch.android.app/25.3.0/2503006",
+  "Dalvik/2.1.0 (Linux; Android 16; SM-X716N Build/UP1A.231005.007) tv.twitch.android.app/25.3.0/2503006",
+  "Dalvik/2.1.0 (Linux; U; Android 15; SM-G990B Build/AP3A.240905.015.A2) tv.twitch.android.app/25.3.0/2503006",
+  "Dalvik/2.1.0 (Linux; U; Android 15; SM-G970F Build/AP3A.241105.008) tv.twitch.android.app/25.3.0/2503006",
+  "Dalvik/2.1.0 (Linux; U; Android 15; SM-A566E Build/AP3A.240905.015.A2) tv.twitch.android.app/25.3.0/2503006",
+  "Dalvik/2.1.0 (Linux; U; Android 14; SM-X306B Build/UP1A.231005.007) tv.twitch.android.app/25.3.0/2503006",
+];
+
+const DEFAULT_TWITCH_USER_AGENT = TWITCH_ANDROID_USER_AGENTS[0];
+const pickUserAgent = (list: string[]) =>
+  list[Math.floor(Math.random() * list.length)] || DEFAULT_TWITCH_USER_AGENT;
+
 // User-Agent matching the Android app client.
 export const TWITCH_WEB_USER_AGENT =
-  "Dalvik/2.1.0 (Linux; U; Android 16; SM-S911B Build/TP1A.220624.014) tv.twitch.android.app/25.3.0/2503006";
+  process.env.TWITCH_WEB_USER_AGENT ?? pickUserAgent(TWITCH_ANDROID_USER_AGENTS);
 
 // Twitch Integrity endpoint (used to obtain Client-Integrity header for GQL when using the web client)
 export const TWITCH_INTEGRITY_URL = "https://gql.twitch.tv/integrity";
