62bd164cf3a17047b7f2d9f65b320da876544899
rxm <33541942+rxm96@users.noreply.github.com>
2026-01-21 19:26:55 +0100

    Revert "Merge pull request #6 from rxm96/feat/autostart"
    This reverts commit 69e4804ea12508f5a6a55edc39a650d3147fe9ff, reversing
    changes made to 49dd55b8ab25cd31ee53f11d29f009063cfb492d.

--- src/main/core/autoStart.ts
+++
@@ -1,9 +0,0 @@
-import { app } from "electron";
-
-export function applyAutoStartSetting(enabled: boolean) {
-  if (process.platform !== "win32") return;
-  app.setLoginItemSettings({
-    openAtLogin: enabled,
-    path: app.getPath("exe"),
-  });
-}
