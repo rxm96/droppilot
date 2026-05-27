23de22a3b478f9256cf31ccbedd3a47ed5c07985
rxm <xmari0@icloud.com>
2026-05-27 19:59:15 +0200

    chore(chrome): add barrel export for chrome components

---
+++ src/renderer/shared/components/chrome/index.ts
@@ -0,0 +1,8 @@
+export { Titlebar } from "./Titlebar";
+export type { TitlebarProps, TitlebarTheme } from "./Titlebar";
+
+export { AppNav } from "./AppNav";
+export type { AppNavProps, AppNavItem, AppNavView } from "./AppNav";
+
+export { Statusbar } from "./Statusbar";
+export type { StatusbarProps, StatusbarItem, StatusbarTone } from "./Statusbar";
